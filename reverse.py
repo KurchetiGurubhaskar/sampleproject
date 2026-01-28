07c23bcb34710d698a823c386abde642b3c79728
KURCHETI GURUBHSKAR <gurubhaskarkurcheti@gmail.com>
2026-01-28 15:51:48 +0530

    reverse.py added

---
+++ reverse.py
@@ -0,0 +1,7 @@
+''' write a program to find the reverse of the given number'''
+num=int(input("enter the number:"))
+rev=0
+while(num>0):
+    rev=rev*10+num%10
+    num//=10
+print("reverse number:",rev)
