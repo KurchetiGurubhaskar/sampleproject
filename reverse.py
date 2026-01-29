e5bd57ff8061eda35131e2167a5cbec136946bb9
KURCHETI GURUBHSKAR <gurubhaskarkurcheti@gmail.com>
2026-01-29 15:24:47 +0530

    Refactor number reversal and palindrome checking
    Refactored the program to include functions for reversing a number and checking for palindromes. Added a function to get all palindromic numbers within a specified range.

--- reverse.py
+++ reverse.py
@@ -1,7 +1,23 @@
 ''' write a program to find the reverse of the given number'''
-num=int(input("enter the number:"))
-rev=0
-while(num>0):
-    rev=rev*10+num%10
-    num//=10
-print("reverse number:",rev)
+def reverse(n):
+   rev=0
+   while(n>0):
+    rev=rev*10+n%10;
+    n//=10
+   return rev
+
+def isPalindrome(num):
+    return num==reverse(num)
+
+print(reverse(123))
+print(isPalindrome(123))
+print(reverse(121))
+print(isPalindrome(121))
+
+def getPalindromes(start,end):
+   res=" "
+   for i in range (start ,end+1):
+      if isPalindrome(i):
+         res=res+str(i)+","
+   return res
+print(getPalindromes(1,1000))
